eb85f28f541ff06bc73ec7c9ff39ef66afef050a
tolltech <alexandrovpe@gmail.com>
2019-12-06 17:34:06 +0400

    remove script errors on not sentry tabs

--- background.js
+++ background.js
@@ -5,15 +5,18 @@
 //     }
 // });
 
-chrome.tabs.onUpdated.addListener(function (tabId){
-    chrome.tabs.executeScript(tabId, {file: "jquery.js"});
-    chrome.tabs.executeScript(tabId, {file: "trigger.js"});
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (!tab || tab.url.indexOf('sentry.skbkontur.ru/auth/login/sentry') == -1) {
+        return;
+    }
+    chrome.tabs.executeScript(tabId, { file: "jquery.js" });
+    chrome.tabs.executeScript(tabId, { file: "trigger.js" });
 });
 
-chrome.tabs.query( {url:["*://billy-sentry.skbkontur.ru/auth/login/sentry/*"]} ,function (tabs) {
+chrome.tabs.query({ url: ["*://billy-sentry.skbkontur.ru/auth/login/sentry/*"] }, function (tabs) {
     for (var i = 0; i < tabs.length; i++) {
-        chrome.tabs.executeScript(tabs[i].id, {file: "jquery.js"});
-        chrome.tabs.executeScript(tabs[i].id, {file: "trigger.js"});
+        chrome.tabs.executeScript(tabs[i].id, { file: "jquery.js" });
+        chrome.tabs.executeScript(tabs[i].id, { file: "trigger.js" });
     }
 });
 
