5947e7b30ed4ae9891f09d0a57d9ad21fdb1d333
tolltech <alexandrovpe@gmail.com>
2020-01-13 10:21:50 +0400

    1.0.6 add jhub.skbkontur.ru

--- background.js
+++ background.js
@@ -11,6 +11,7 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         (
             tab.url.indexOf('sentry.skbkontur.ru/auth/login/sentry') == -1
             && tab.url.indexOf('sentry.testkontur.ru/auth/login/sentry') == -1
+            && tab.url.indexOf('jhub.skbkontur.ru/hub/auth/login') == -1
         )) {
         return;
     }
@@ -18,7 +19,11 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     chrome.tabs.executeScript(tabId, { file: "trigger.js" });
 });
 
-chrome.tabs.query({ url: ["*://billy-sentry.skbkontur.ru/auth/login/sentry/*", "*://billy-sentry.testkontur.ru/auth/login/sentry/*"] }, function (tabs) {
+chrome.tabs.query({ url: [
+    "*://billy-sentry.skbkontur.ru/auth/login/sentry/*",
+     "*://billy-sentry.testkontur.ru/auth/login/sentry/*",
+     "*://jhub.skbkontur.ru/hub/auth/login*"
+    ] }, function (tabs) {
     for (var i = 0; i < tabs.length; i++) {
         chrome.tabs.executeScript(tabs[i].id, { file: "jquery.js" });
         chrome.tabs.executeScript(tabs[i].id, { file: "trigger.js" });
